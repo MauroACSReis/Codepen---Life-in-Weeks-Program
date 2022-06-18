0426a05bc2a5c75e361470bb580dab3e55e35d94
Mauro Reis <mauroacsreis@gmail.com>
2022-06-18 10:23:08 -0300

    Create main.js
    Added two solutions

---
+++ main.js
@@ -0,0 +1,17 @@
+var age = prompt("Enter your age");
+var yearsRemaining = 90 - age;
+
+var days = (yearsRemaining*365);
+var weeks = (yearsRemaining*52);
+var months = (yearsRemaining*12);
+
+console.log("You have " + days + " days, " + weeks + " weeks, " + months + " months.")
+
+OR
+
+var yearsRemaining = 90 - age;
+var days = (yearsRemaining*365);
+var weeks = (yearsRemaining*52);
+var months = (yearsRemaining*12);
+
+console.log("You have " + days + " days, " + weeks + " weeks, and " + months + " months left.")
